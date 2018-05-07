c203f5683ed8601e571bd0ef4296ba092a942f6e
qunwang <niuyiwangqun@sohu.com>
2018-05-07 22:26:21 +0800

    init

---
+++ test/ouput.test.js
@@ -0,0 +1,9 @@
+/* eslint-disable   no-console */
+// 测试输出
+const clc = require('cli-color');
+
+const checker = require('../lib/checker');
+
+let result = checker(`【测试页】这是个测试】`);
+
+console.log('ouput.test.js');
